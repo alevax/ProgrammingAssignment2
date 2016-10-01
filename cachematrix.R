0819a2890d9a5d5499a5876849d5dc56c96857e3
Ing. Alessandro Vasciaveo <alessandro.vasciaveo@gmail.com>
2016-10-01 14:31:07 -0400

    Done and tested
    It’s working and there is a test function at the end of the source.

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,24 @@
+# ---
 ## Put comments here that give an overall description of what your
 ## functions do
 
 ## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function( .matrix = matrix() ) 
+{
+  .inverse <- NULL
+  set <- function(myMatrix) {
+    .matrix <<- myMatrix
+    .inverse <<- NULL
+  }
+  get <- function() .matrix
+  
+  setInverse <- function(myInverse) .inverse <<- myInverse
+  getInverse <- function() .inverse
+  
+  res <- list(set = set, get = get,
+              setInverse = setInverse,
+              getInverse = getInverse)
+  return(res)
 }
 
 
@@ -12,4 +26,26 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  inverse <- x$getInverse()
+  if(!is.null(inverse)) {
+    message("getting cached data")
+    return(inverse)
+  }
+  data <- x$get()
+  inverse <- solve(data, ...)
+  x$setInverse(inverse)
+  return(inverse)
 }
+
+##
+# Running example
+# ---------------
+  set.seed(1986) # set seed for reproductibility
+  nrows = 1000 # number of rows (and columns)
+  mat <- matrix( runif(nrows^2) , ncol=nrows , nrow=nrows ) # fill a matrix with random data
+
+  mat.cached <- makeCacheMatrix(mat) # create the matrix
+  y <- cacheSolve(mat.cached) # solve for the inverse
+  head(y) # check the result
+
+  
